1badadd3a8fe941fa2d0501b10e3c13c3bcd3126
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-12-10 17:51:05 -0500

    🐛 fix navbar position

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -5,16 +5,18 @@ import { Navbar } from "./navbar";
 
 export function Header() {
   return (
-    <header className="flex justify-center mx-auto py-5 lg:w-10/12 lg:justify-between">
-      <div className="lg:w-1/4">
-        <Logo />
-      </div>
-      <Navbar />
-      <div className="lg:w-1/4">
-        <Button className="hidden lg:flex lg:ml-auto">
-          Falar com especialista
-          <ArrowRight />
-        </Button>
+    <header className="absolute z-10 w-full">
+      <div className="flex justify-center mx-auto py-5 lg:w-10/12 lg:justify-between">
+        <div className="lg:w-1/4">
+          <Logo />
+        </div>
+        <Navbar />
+        <div className="lg:w-1/4">
+          <Button className="hidden lg:flex lg:ml-auto">
+            Falar com especialista
+            <ArrowRight />
+          </Button>
+        </div>
       </div>
     </header>
   );
