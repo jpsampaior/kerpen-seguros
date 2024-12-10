631bb27e27a7e516dda53c3503d2114f81195361
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-12-10 15:08:29 -0500

    🎉 init

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F0C05D",
+        secondary: "#2B3864",
+        tertiary: "#6C718E",
+        background: {
+          default: "#20201F",
+          secondary: "#252B42",
+        },
+        foreground: "#F6F3F4",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+export default config;
