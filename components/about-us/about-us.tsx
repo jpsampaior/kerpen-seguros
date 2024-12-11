3c9562c584723dae3076dbf6a310350ae786e554
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-12-10 20:43:55 -0500

    📱 responsive about-us

--- components/about-us/about-us.tsx
+++ components/about-us/about-us.tsx
@@ -23,24 +23,31 @@ const partners = [
 
 export function AboutUs() {
   return (
-    <section className="bg-secondary/30 py-14 text-center">
+    <section className="bg-secondary/30 py-14 text-center space-y-6 lg:space-y-0">
       <h2 className="text-4xl font-bold">Quem Somos</h2>
       <div className="space-y-5">
         {partners.map((partner, index) => (
-          <div key={index} className="flex items-center gap-5 w-8/12 mx-auto">
+          <div
+            key={index}
+            className="flex flex-col items-center gap-5 w-8/12 mx-auto lg:flex-row"
+          >
             <div className="rounded-full bg-stone-600 w-fit">
               <Avatar className="w-[150px] h-[150px]">
                 <AvatarImage src="/avatar-placeholder.png" />
                 <AvatarFallback>OK</AvatarFallback>
               </Avatar>
             </div>
-            <div className="text-start">
-              <div className="flex items-baseline gap-3 text-primary">
-                <h3 className="text-2xl font-bold">{partner.name}</h3>
-                <h4>{partner.role}</h4>
+            <div className="text-start space-y-2">
+              <div>
+                <div className="flex justify-center items-baseline gap-3 text-primary lg:justify-start">
+                  <h3 className="text-2xl font-bold">{partner.name}</h3>
+                  <h4>{partner.role}</h4>
+                </div>
+                <p className="text-center lg:text-start">
+                  {partner.description}
+                </p>
               </div>
-              <div>{partner.description}</div>
-              <div className="flex gap-2">
+              <div className="flex flex-col items-center gap-2 lg:flex-row lg:items-start">
                 <Link
                   href={`mailto:${partner.email}`}
                   className="flex gap-2 items-center hover:text-primary transition-colors"
