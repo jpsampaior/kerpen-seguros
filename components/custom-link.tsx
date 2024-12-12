481dd71af4fb4f3df128660c47a96531b99f3cce
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-12-12 00:37:04 -0500

    🚨 fix errors to build

--- components/custom-link.tsx
+++ components/custom-link.tsx
@@ -4,12 +4,19 @@ import Link, { LinkProps } from "next/link";
 interface CustomLinkProps extends LinkProps {
   className?: string;
   children: React.ReactNode;
+  target?: string;
 }
 
-export function CustomLink({ className, children, ...props }: CustomLinkProps) {
+export function CustomLink({
+  className,
+  children,
+  target,
+  ...props
+}: CustomLinkProps) {
   return (
     <Link
       {...props}
+      target={target}
       className={cn(
         "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors bg-primary text-stone-50 hover:bg-primary/85 h-9 px-4 py-2 w-fit",
         className
