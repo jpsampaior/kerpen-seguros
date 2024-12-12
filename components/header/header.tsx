8dbb96e7fdb4740c5be0901d42f518a798417238
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-12-11 22:52:50 -0500

    ✨ navbar movement

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,11 +1,48 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { ArrowRight } from "lucide-react";
 import { Button } from "../ui/button";
 import { Logo } from "./logo";
 import { Navbar } from "./navbar";
 
 export function Header() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+
+      setHasScrolled(currentScrollY > 0);
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <header className="absolute z-10 w-full">
+    <motion.header
+      initial={{ y: 0 }}
+      animate={{ y: isVisible ? 0 : -100 }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+      className={`fixed z-10 w-full bg-background-default ${
+        hasScrolled ? "shadow-md" : ""
+      }`}
+    >
       <div className="flex justify-center mx-auto py-5 lg:w-10/12 lg:justify-between">
         <div className="lg:w-1/4">
           <Logo />
@@ -18,6 +55,6 @@ export function Header() {
           </Button>
         </div>
       </div>
-    </header>
+    </motion.header>
   );
 }
