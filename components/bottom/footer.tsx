4ae0a540528354278960c430cd1bc8448f38f8a2
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-12-13 18:59:11 -0500

    ✨ whatsapp icon mobile

--- components/bottom/footer.tsx
+++ components/bottom/footer.tsx
@@ -1,9 +1,11 @@
 import { Advertisement } from "./advertisement";
 import { FooterContent } from "./footer-content";
+import { WhatsappIcon } from "./whatsapp-icon";
 
 export function Footer() {
   return (
     <footer className="bg-background-tertiary">
+      <WhatsappIcon />
       <Advertisement />
       <FooterContent />
     </footer>
---
+++ components/bottom/whatsapp-icon.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export function WhatsappIcon() {
+  return (
+    <Link
+      href="https://wa.me/1234567890"
+      target="_blank"
+      className="fixed bottom-4 right-4 sm:hidden z-50"
+    >
+      <Image
+        src="/whatsapp-icon.png"
+        alt="WhatsApp Icon"
+        width={60}
+        height={60}
+        className="hover:scale-110 transition-transform"
+      />
+    </Link>
+  );
+}
