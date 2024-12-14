7345a96ef332a9db856d572a3390166e03768823
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-12-13 19:03:06 -0500

    🐛 fix whatsapp number

--- components/bottom/whatsapp-icon.tsx
+++ components/bottom/whatsapp-icon.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function WhatsappIcon() {
   return (
     <Link
-      href="https://wa.me/1234567890"
+      href="https://wa.me/5585997646804"
       target="_blank"
       className="fixed bottom-4 right-4 sm:hidden z-50"
     >
